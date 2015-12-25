8c71a1e0d4b0be230ba7fb0ef45c01bf29058d5b
Yongquan Lu <yqlu@mit.edu>
2015-12-25 21:53:17 +0800

    more dependencies

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,3 @@
-function clearCurrentLink(){
-	var a = document.getElementsByTagName("A");
-	for(var i=0;i<a.length;i++){
-		if(a[i].href.split("#")[0] == window.location.href.split("#")[0]){
-			removeNode(a[i]);
-		}
-	}
-}
-
-function removeNode(n){
-	if(n.hasChildNodes()){
-		for(var i=0;i<n.childNodes.length;i++){
-			n.parentNode.insertBefore(n.childNodes[i].cloneNode(true),n);
-		}
-	}
-	n.parentNode.removeChild(n);
-}
-
-$(document).ready(function() {
-	var $grid = $('.grid').masonry({
-		percentPosition: true
-	});
-
-	$grid.imagesLoaded().progress( function() {
-		$grid.masonry('layout');
-	});
-});
-
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
